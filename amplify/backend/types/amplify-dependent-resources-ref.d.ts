86686bd7a1107cba1f36ce36331c76e5983082f3
Luis Cruz <lcruz@cementoscibao.com>
2024-04-19 16:36:40 -0400

    Modulo 4 terminado

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappdf96f73f": {
       "AppClientID": "string",
